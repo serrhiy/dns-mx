eecdc57d246c43ffad221ddb0f8eedf3ba6498a0
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-12-22 14:48:55 +0000

    Add timeout processing

--- src/asio/main.cc
+++ src/asio/main.cc
@@ -1,8 +1,5 @@
 #include <print>
-#include <unordered_map>
-#include <iostream>
 #include <vector>
-#include <ranges>
 #include <future>
 #include <map>
 
@@ -16,43 +13,76 @@ struct MxRecord {
   short preference;
 };
 
+constexpr int DEFAULT_TIMEOUT = 2000;
+
 class MxResolver {
+  using wait_type = boost::asio::posix::descriptor_base::wait_type;
+
   ares_channel_t* channel;
   boost::asio::any_io_executor executor;
+  boost::asio::steady_timer timeout_timer;
   std::map<int, boost::asio::posix::stream_descriptor> fds;
 
   static void socketStateCallback(void *data, ares_socket_t fd, int readable, int writable) {
     MxResolver* resolver = reinterpret_cast<MxResolver*>(data);
     auto callback = std::bind(&MxResolver::onSocketEvent, resolver, fd, readable != 0, writable != 0);
-    boost::asio::dispatch(resolver->executor, std::move(callback));
+    boost::asio::post(resolver->executor, std::move(callback));
+  }
+
+  void updateTimer() {
+    using namespace boost::asio::chrono;
+
+    struct timeval tv;
+    ares_timeout(channel, nullptr, &tv);
+    timeout_timer.cancel();
+    timeout_timer.expires_after(seconds{ tv.tv_sec } + microseconds{ tv.tv_usec });
+    timeout_timer.async_wait([this](const boost::system::error_code& err) {
+      if (!err) {
+        ares_process_fd(channel, ARES_SOCKET_BAD, ARES_SOCKET_BAD);
+      }
+    });
+  }
+
+  void asyncSocketEvent(const boost::system::error_code& err, int fd, wait_type type) {
+    using boost::asio::posix::stream_descriptor;
+
+    const auto descriptor = fds.find(fd);
+    if (err || descriptor == fds.end()) {
+      return;
+    }
+    const int read_fd = type == wait_type::wait_write ? ARES_SOCKET_BAD : fd;
+    const int write_fd = type == wait_type::wait_write ? fd : ARES_SOCKET_BAD;
+    ares_process_fd(channel, read_fd, write_fd);
+    updateTimer();
+    auto callback = std::bind(&MxResolver::asyncSocketEvent, this, boost::asio::placeholders::error, fd, type);
+    descriptor->second.async_wait(type, std::move(callback));
   }
 
   void onSocketEvent(int fd, bool readable, bool writable) {
-    using boost::asio::posix::stream_descriptor::wait_read;
-    using boost::asio::posix::stream_descriptor::wait_write;
-
     if (!readable && !writable) {
-      if (fds.contains(fd)) {
-        fds.erase(fd);
+      const auto descriptor = fds.find(fd);
+      if (descriptor != fds.end()) {
+        fds.erase(descriptor);
       }
       return;
     }
 
+    updateTimer();
+
     auto& descriptor = fds.try_emplace(fd, executor, fd).first->second;
     if (readable) {
-      descriptor.async_wait(wait_read, [this, fd](const boost::system::error_code& err) {
-        if (!err) {
-          ares_process_fd(channel, fd, ARES_SOCKET_BAD);
-        }
+      descriptor.async_wait(wait_type::wait_read, [this, fd](const boost::system::error_code& err) {
+        asyncSocketEvent(err, fd, wait_type::wait_read);
       });
     }
     if (writable) {
-      descriptor.async_wait(wait_write, [this, fd](const boost::system::error_code& err) {
-        if (!err) {
-          ares_process_fd(channel, ARES_SOCKET_BAD, fd);
-        }
+      descriptor.async_wait(wait_type::wait_write, [this, fd](const boost::system::error_code& err) {
+        asyncSocketEvent(err, fd, wait_type::wait_write);
       });
     }
+    descriptor.async_wait(wait_type::wait_error, [this, fd](const boost::system::error_code& err) {
+      asyncSocketEvent(err, fd, wait_type::wait_error);
+    });
   }
 
   static void processResponse(void* arg, ares_status_t status, size_t timeouts, const ares_dns_record_t* dnsrec) {
@@ -73,17 +103,20 @@ class MxResolver {
     std::vector<MxRecord> records;
     for (size_t index = 0; index < count; index++) {
       ares_dns_rr_t* record = ares_dns_record_rr_get(const_cast<ares_dns_record_t*>(dnsrec), ARES_SECTION_ANSWER, index);
-      const char* mx = ares_dns_rr_get_str(record, ARES_RR_MX_EXCHANGE);
       const short preference = ares_dns_rr_get_u16(record, ARES_RR_MX_PREFERENCE);
-      records.push_back(MxRecord{mx, preference});
+      const char* mx = ares_dns_rr_get_str(record, ARES_RR_MX_EXCHANGE);
+      if (mx != NULL) {
+        records.push_back(MxRecord{mx, preference});
+      }
     }
     promise->set_value(std::move(records));
   }
 
   public:
-  explicit MxResolver(boost::asio::any_io_executor executor): channel{ nullptr }, executor{ executor } {
-    ares_options options{ .sock_state_cb = socketStateCallback, .sock_state_cb_data = this };
-    int optmask = ARES_OPT_SOCK_STATE_CB;
+  explicit MxResolver(boost::asio::any_io_executor executor, int timeout = DEFAULT_TIMEOUT):
+      channel{ nullptr }, executor{ executor }, timeout_timer{ executor } {
+    ares_options options{ .timeout = timeout, .sock_state_cb = socketStateCallback, .sock_state_cb_data = this };
+    int optmask = ARES_OPT_SOCK_STATE_CB | ARES_OPT_TIMEOUTMS;
     if (int status = ares_init_options(&channel, &options, optmask); status != ARES_SUCCESS) {
       throw std::runtime_error{ ares_strerror(status) };
     }
@@ -98,9 +131,10 @@ class MxResolver {
   MxResolver& operator=(MxResolver&&) = delete;
 
   std::future<std::vector<MxRecord>> request(std::string_view domain) {
-    auto promise = new std::promise<std::vector<MxRecord>>{};
+    auto promise = new std::promise<std::vector<MxRecord>>;
+    auto future = promise->get_future();
     ares_query_dnsrec(channel, domain.data(), ARES_CLASS_IN, ARES_REC_TYPE_MX, processResponse, promise, nullptr);
-    return promise->get_future();
+    return future;
   }
 
   ~MxResolver() {
@@ -111,14 +145,16 @@ class MxResolver {
 int main(const int argc, const char* argv[]) {
   boost::asio::io_context io_context;
   MxResolver resolver{ io_context.get_executor() };
-  auto future = resolver.request("gmail.com1");
-  io_context.run();
-  const auto records = future.get();
-  if (records.empty()) {
-    std::print("No records found for such domain\n");
+  const std::vector<std::string> domains{ "gmail.com", "jci.com", "outlook.com" };
+  std::vector<std::future<std::vector<MxRecord>>> futures;
+  for (const std::string& domain: domains) {
+    futures.push_back(resolver.request(domain));
   }
-  for (const auto [hostname, preference]: records) {
-    std::print("MX: {}, Preference: {}\n", hostname, preference);
+  io_context.run();
+  for (auto& future: futures) {
+    for (const auto& [hostname, preference]: future.get()) {
+      std::println("Mx: {}, Preference: {}", hostname, preference);
+    }
   }
   return 0;
 }
