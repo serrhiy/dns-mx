236d6854bd035e15a3fde906d2bfa6bcbd03666f
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-12-23 11:46:03 +0000

    Add thread safety

--- src/asio/main.cc
+++ src/asio/main.cc
@@ -3,6 +3,7 @@
 #include <future>
 #include <map>
 #include <functional>
+#include <memory>
 
 #include <ares.h>
 #include <boost/asio.hpp>
@@ -20,32 +21,34 @@ class MxResolver {
   using callback_t = std::function<void(std::exception_ptr, result_t)>;
 
   ares_channel_t* channel;
-  boost::asio::any_io_executor executor;
+  boost::asio::strand<boost::asio::any_io_executor> strand;
   boost::asio::steady_timer timeout_timer;
   std::map<int, boost::asio::posix::stream_descriptor> fds;
 
   static void socketStateCallback(void *data, ares_socket_t fd, int readable, int writable) {
     MxResolver* resolver = reinterpret_cast<MxResolver*>(data);
-    auto callback = std::bind(&MxResolver::onSocketEvent, resolver, fd, readable != 0, writable != 0);
-    boost::asio::dispatch(resolver->executor, std::move(callback));
+    boost::asio::dispatch(resolver->strand, [resolver, fd, readable, writable]() {
+      resolver->onSocketEvent(fd, readable != 0, writable != 0);
+    });
   }
 
   void updateTimer() {
-    using namespace boost::asio::chrono;
-
     timeout_timer.cancel();
-    if (fds.empty()) {
+    struct timeval tv;
+    if (ares_timeout(channel, nullptr, &tv) == nullptr) {
       return;
     }
 
-    struct timeval tv;
-    ares_timeout(channel, nullptr, &tv);
-    timeout_timer.expires_after(seconds{ tv.tv_sec } + microseconds{ tv.tv_usec });
-    timeout_timer.async_wait([this](const boost::system::error_code& err) {
-      if (!err) {
-        ares_process_fd(channel, ARES_SOCKET_BAD, ARES_SOCKET_BAD);
-      }
-    });
+    timeout_timer.expires_after(std::chrono::seconds{ tv.tv_sec } +
+                                std::chrono::microseconds{ tv.tv_usec });
+
+    timeout_timer.async_wait(boost::asio::bind_executor(strand, 
+      [this](const boost::system::error_code& err) {
+        if (!err) {
+          ares_process_fd(channel, ARES_SOCKET_BAD, ARES_SOCKET_BAD);
+          updateTimer();
+        }
+      }));
   }
 
   void asyncSocketEvent(const boost::system::error_code& err, int fd, wait_type type) {
@@ -59,8 +62,20 @@ class MxResolver {
     const int write_fd = type == wait_type::wait_write ? fd : ARES_SOCKET_BAD;
     ares_process_fd(channel, read_fd, write_fd);
     updateTimer();
-    auto callback = std::bind(&MxResolver::asyncSocketEvent, this, boost::asio::placeholders::error, fd, type);
-    descriptor->second.async_wait(type, std::move(callback));
+    if (type != wait_type::wait_error) {
+      startAsyncWait(fd, type);
+    }
+  }
+
+  void startAsyncWait(int fd, wait_type type) {
+    const auto descriptor = fds.find(fd);
+    if (descriptor == fds.end()) {
+      return;
+    }
+    descriptor->second.async_wait(type, boost::asio::bind_executor(strand,
+      [this, fd, type](const boost::system::error_code& err) {
+        asyncSocketEvent(err, fd, type);
+      }));
   }
 
   void onSocketEvent(int fd, bool readable, bool writable) {
@@ -72,22 +87,16 @@ class MxResolver {
       return;
     }
 
-    auto& descriptor = fds.try_emplace(fd, executor, fd).first->second;
+    auto& descriptor = fds.try_emplace(fd, strand.get_inner_executor(), fd).first->second;
     updateTimer();
-
+    
     if (readable) {
-      descriptor.async_wait(wait_type::wait_read, [this, fd](const boost::system::error_code& err) {
-        asyncSocketEvent(err, fd, wait_type::wait_read);
-      });
+      startAsyncWait(fd, wait_type::wait_read);
     }
     if (writable) {
-      descriptor.async_wait(wait_type::wait_write, [this, fd](const boost::system::error_code& err) {
-        asyncSocketEvent(err, fd, wait_type::wait_write);
-      });
+      startAsyncWait(fd, wait_type::wait_write);
     }
-    descriptor.async_wait(wait_type::wait_error, [this, fd](const boost::system::error_code& err) {
-      asyncSocketEvent(err, fd, wait_type::wait_error);
-    });
+    startAsyncWait(fd, wait_type::wait_error);
   }
 
   static void processResponse(void* arg, ares_status_t status, size_t timeouts, const ares_dns_record_t* dnsrec) {
@@ -118,7 +127,7 @@ class MxResolver {
 
   public:
   explicit MxResolver(boost::asio::any_io_executor executor, int timeout = DEFAULT_TIMEOUT):
-      channel{ nullptr }, executor{ executor }, timeout_timer{ executor } {
+      channel{ nullptr }, strand{ executor }, timeout_timer{ executor } {
     ares_options options{ .timeout = timeout, .sock_state_cb = socketStateCallback, .sock_state_cb_data = this };
     int optmask = ARES_OPT_SOCK_STATE_CB | ARES_OPT_TIMEOUTMS;
     if (int status = ares_init_options(&channel, &options, optmask); status != ARES_SUCCESS) {
@@ -162,6 +171,19 @@ int main(const int argc, const char* argv[]) {
   MxResolver resolver{ io_context.get_executor() };
   
   const std::vector<std::string> domains{ "gmail.com", "jci.com", "outlook.com" };
+
+  // for (const std::string& domain: domains) {
+  //   resolver.request(domain, [](std::exception_ptr exception, std::vector<MxRecord> result) {
+  //     if (exception) {
+  //       std::println("error occured");
+  //     }
+  //     for (const auto& [hostname, preference]: result) {
+  //       std::println("Mx: {}, Preference: {}", hostname, preference);
+  //     }
+  //   });
+  // }
+  // io_context.run();
+
   std::vector<std::future<std::vector<MxRecord>>> futures;
   for (const std::string& domain: domains) {
     futures.push_back(resolver.request(domain));
